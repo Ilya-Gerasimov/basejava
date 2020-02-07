aab7f05eaf0cea87be159630903338661db7a646
Ilya <gim78@yandex.ru>
2020-02-07 23:00:11 +0300

    HW1-1

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -29,8 +29,8 @@ public class ArrayStorage {
     void delete(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
-                storage[i] = storage[size - 1];
                 size--;
+                storage[i] = storage[size];
                 storage[size] = null;
                 break;
             }
