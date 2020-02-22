e7646b8951829676165de5af8532ac959fbbfe35
Ilya <gim78@yandex.ru>
2020-02-22 18:47:19 +0300

    HW8 add Positions inTo Organization

--- src/com/urise/webapp/model/Organization.java
+++ src/com/urise/webapp/model/Organization.java
@@ -1,64 +1,75 @@
 package com.urise.webapp.model;
 
 import java.time.YearMonth;
+import java.util.List;
 import java.util.Objects;
 
 public class Organization {
-    private final String name;
-    private final String url;
-    private final String title;
-    private final String description;
-    private final YearMonth startDate;
-    private final YearMonth endDate;
+    private final Link homePage;
+    private List<Position> positions;
 
-    public Organization(String name, String url, String title, String description, YearMonth startDate, YearMonth endDate) {
+    public Organization(String name, String url, List<Position> positions) {
         Objects.requireNonNull(name, "name cannot be null");
-        Objects.requireNonNull(title, "title cannot be null");
-        Objects.requireNonNull(startDate, "startDate cannot be null");
-        Objects.requireNonNull(endDate, "endDate cannot be null");
-        this.name = name;
-        this.url = url;
-        this.title = title;
-        this.description = description;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
+        this.homePage = new Link(name, url);
+        this.positions = positions;
+     }
 
     @Override
     public String toString() {
         return "\nOrganization{" +
-                "name='" + name + '\'' +
-                ", url='" + url + '\'' +
-                ", title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", startDate=" + startDate +
-                ", endDate=" + endDate +
+                "homePage=" + homePage +
+                ", position=" + positions +
                 '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
 
-        Organization that = (Organization) o;
+    public static class Position {
+        private final String title;
+        private final String description;
+        private final YearMonth startDate;
+        private final YearMonth endDate;
 
-        if (!name.equals(that.name)) return false;
-        if (url != null ? url.equals(that.url) : that.url != null) return false;
-        if (!title.equals(that.title)) return false;
-        if (description != null ? description.equals(that.description) : that.description != null) return false;
-        if (!startDate.equals(that.startDate)) return false;
-        return endDate.equals(that.endDate);
-    }
+        public Position(String title, String description, YearMonth startDate, YearMonth endDate) {
+            Objects.requireNonNull(title, "title cannot be null");
+            Objects.requireNonNull(startDate, "startDate cannot be null");
+            Objects.requireNonNull(endDate, "endDate cannot be null");
+            this.title = title;
+            this.description = description;
+            this.startDate = startDate;
+            this.endDate = endDate;
+        }
 
-    @Override
-    public int hashCode() {
-        int result = name.hashCode();
-        result = 31 * result + (url != null ? url.hashCode() : 0);
-        result = 31 * result + title.hashCode();
-        result = 31 * result + (description != null ? description.hashCode() : 0);
-        result = 31 * result + startDate.hashCode();
-        result = 31 * result + endDate.hashCode();
-        return result;
+        @Override
+        public String toString() {
+            return "\nPosition{" +
+                    "title='" + title + '\'' +
+                    ", description='" + description + '\'' +
+                    ", startDate=" + startDate +
+                    ", endDate=" + endDate +
+                    '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            Position position = (Position) o;
+
+            if (!title.equals(position.title)) return false;
+            if (description != null ? description.equals(position.description) : position.description != null)
+                return false;
+            if (!startDate.equals(position.startDate)) return false;
+            return endDate.equals(position.endDate);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = title.hashCode();
+            result = 31 * result + (description != null ? description.hashCode() : 0);
+            result = 31 * result + startDate.hashCode();
+            result = 31 * result + endDate.hashCode();
+            return result;
+        }
     }
 }
