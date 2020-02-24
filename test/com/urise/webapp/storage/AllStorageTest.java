f6a4221576c863f5f9d5716960477e4a381048f9
Ilya <gim78@yandex.ru>
2020-02-24 16:29:24 +0300

    HW9 realized ObjectStreamPathStorage and Test

--- test/com/urise/webapp/storage/AllStorageTest.java
+++ test/com/urise/webapp/storage/AllStorageTest.java
@@ -10,7 +10,9 @@ import org.junit.runners.Suite;
                 SortedArrayStorageTest.class,
                 ListStorageTest.class,
                 MapUuidStorageTest.class,
-                MapStorageTest.class
+                MapStorageTest.class,
+                ObjectStreamStorageTest.class,
+                ObjectStreamPathStorageTest.class
         })
 
 public class AllStorageTest {
