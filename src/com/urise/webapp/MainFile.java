00dccddc2b0c055bac6a99dc3a20df5a26db97fd
Ilya <gim78@yandex.ru>
2020-02-25 14:28:14 +0300

    HW9

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -31,27 +31,46 @@ public class MainFile {
             throw new RuntimeException(e);
         }
 
-        // Вторая часть ДЗ. Файлы директории выводились вперемешку с директориями, поэтому вынес в отдельный цикл
+        // Вторая часть ДЗ8. Файлы директории выводились вперемешку с директориями, поэтому вынес в отдельный цикл
 
-        File folder = new File("./src/com/urise/webapp");
+//        File folder = new File("./src/com/urise/webapp");
+//
+//        for (File files : folder.listFiles()) {
+//            if (files != null) {
+//                if (files.isDirectory()) {
+//                    System.out.println(files.getName());
+//                    for (String d : files.list()) {
+//                        System.out.println("     " + d);
+//                    }
+//                }
+//            }
+//        }
+//        System.out.println("Файлы текущей директории");
+//
+//        for (File files : folder.listFiles()) {
+//            if (files != null) {
+//                if (!file.isDirectory())
+//                    System.out.println("     " + files.getName());
+//            }
+//        }
 
-        for (File files : folder.listFiles()) {
-            if (files != null) {
-                if (files.isDirectory()) {
-                    System.out.println(files.getName());
-                    for (String d : files.list()) {
-                        System.out.println("     " + d);
-                    }
+        printDirectoryDeeply(dir);
+
+
+    }
+
+    public static void printDirectoryDeeply(File dir) {
+        File[] files = dir.listFiles();
+        if (files != null) {
+            for (File file : files) {
+                if (file.isFile()) {
+                    System.out.println("                File: " + file.getName());
+                } else if (file.isDirectory()) {
+                    System.out.println("Directory: " + file.getName());
+                    printDirectoryDeeply(file);
                 }
             }
         }
-        System.out.println("Файлы текущей директории");
-
-        for (File files : folder.listFiles()) {
-            if (files != null) {
-                if (!file.isDirectory())
-                    System.out.println("     " + files.getName());
-            }
-        }
     }
+
 }
