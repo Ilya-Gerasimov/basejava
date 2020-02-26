bd44d55b2090142b81b93378a675112d22ab3e8f
Ilya <gim78@yandex.ru>
2020-02-26 15:00:48 +0300

    HW9 finish

--- test/com/urise/webapp/storage/ObjectStreamStorageTest.java
+++ test/com/urise/webapp/storage/ObjectStreamStorageTest.java
@@ -1,8 +1,10 @@
 package com.urise.webapp.storage;
 
+import com.urise.webapp.storage.serializer.ObjectStreamSerializer;
+
 public class ObjectStreamStorageTest extends AbstractStorageTest {
 
     public ObjectStreamStorageTest() {
-        super(new FileStorage(STORAGE_DIR));
+        super(new FileStorage(STORAGE_DIR, new ObjectStreamSerializer()));
     }
 }
