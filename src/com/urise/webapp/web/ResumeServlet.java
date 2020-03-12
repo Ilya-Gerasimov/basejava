dac3396cdbdde12a9eea0c4eb8810d14fff74c14
Ilya <gim78@yandex.ru>
2020-03-12 11:41:40 +0300

    HW15 table

---
+++ src/com/urise/webapp/web/ResumeServlet.java
@@ -0,0 +1,59 @@
+package com.urise.webapp.web;
+
+import com.urise.webapp.Config;
+import com.urise.webapp.model.Resume;
+import com.urise.webapp.storage.Storage;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.Writer;
+
+public class ResumeServlet extends HttpServlet {
+    private Storage storage;
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+        storage = Config.get().getStorage();
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+//        response.setHeader("Content-Type", "text/html; charset=UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+//        String name = request.getParameter("name");
+//        response.getWriter().write(name == null ? "Hello Resumes!" : "Hello " + name + '!');
+        Writer writer = response.getWriter();
+        writer.write(
+                "<html>" +
+                        "<head>" +
+                        "    <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">" +
+                        "    <title>Список всех резюме</title>" +
+                        "</head>" +
+                        "<body>" +
+                        "<table border=\"1\" cellpadding=\"8\" cellspacing=\"0\">" +
+                        "    <tr>" +
+                        "        <th>Uuid</th>" +
+                        "        <th>Name</th>" +
+                        "    </tr>\n");
+        for (Resume resume : storage.getAllSorted()) {
+            writer.write(
+                    "<tr>" +
+                            "     <td>" + resume.getUuid() + "</td>" +
+                            "     <td>" + resume.getFullName() + "</td>" +
+                            "</tr>");
+        }
+        writer.write("</table>" +
+                "</body>" +
+                "</html>");
+    }
+}
