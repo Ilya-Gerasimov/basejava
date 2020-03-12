c7795838f224188de7e0a699531e5ee8bbc98453
Ilya <gim78@yandex.ru>
2020-03-12 21:15:55 +0300

    HW15 finish

--- config/init_db.sql
+++ config/init_db.sql
@@ -21,3 +21,11 @@ create table if not exists contact
 
 create unique index contact_uuid_type_index
     on contact (resume_uuid, type);
+
+CREATE TABLE section (
+                         id serial primary key ,
+                         resume_uuid varchar(36) not null references resume (uuid) on delete cascade,
+                         type text not null ,
+                         content text not null
+);
+create unique index section_idx on section (resume_uuid, type);
